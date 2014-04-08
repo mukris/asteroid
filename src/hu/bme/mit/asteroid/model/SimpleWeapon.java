d85484802613af0d1e536ccec4f1482d8d5da815
mukris <kristof.muki@mailbox.com>
2014-04-08 12:05:10 +0200

    Weapon továbbfejlesztése

--- src/hu/bme/mit/asteroid/model/SimpleWeapon.java
+++ src/hu/bme/mit/asteroid/model/SimpleWeapon.java
@@ -8,4 +8,15 @@ public class SimpleWeapon extends Weapon {
 	public SimpleWeapon(Vector2D position, Vector2D speed) {
 		super(position, speed);
 	}
+
+	@Override
+	public SimpleWeapon clone() {
+		return new SimpleWeapon(getPosition(), getSpeed());
+	}
+
+	@Override
+	public boolean isAlive(long currentTime) {
+		// TODO lövedék élettartamának meghatározása (időkülönbség, ill sebesség alapján)
+		return true;
+	}
 }
--- src/hu/bme/mit/asteroid/model/Weapon.java
+++ src/hu/bme/mit/asteroid/model/Weapon.java
@@ -3,12 +3,26 @@ package hu.bme.mit.asteroid.model;
 /**
  * A játékban szereplő fegyvereket reprezentáló absztrakt osztály
  */
-public abstract class Weapon extends DirectionalMovingSpaceObject {
-	
+public abstract class Weapon extends DirectionalMovingSpaceObject implements Cloneable {
+
 	private static final int WEAPON_SIZE = 10;
+	protected long mShotAtTimeMillis = 0;
 
 	public Weapon(Vector2D position, Vector2D speed) {
 		super(position, speed, speed.getDirection(), WEAPON_SIZE);
 	}
 
+	@Override
+	public abstract Weapon clone();
+
+	/**
+	 * Visszaadja, hogy a kérdéses időpillanatban a lövedék még "éles"-e. Ez
+	 * segít a lövedék maximális élettartamát meghatározni, hogy egy kilőtt
+	 * lövedék ne keringjen a végtelenségig.
+	 * 
+	 * @param currentTime
+	 *            Az aktuális idő ms-ban
+	 * @return True, ha még aktív, false ha nem.
+	 */
+	public abstract boolean isAlive(long currentTime);
 }
