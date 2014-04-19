3bf66d312dbcf69d0c0900e62924e6e9c2d88fee
mukris <kristof.muki@mailbox.hu>
2014-04-19 02:26:36 +0200

    bugfix: hibás függvényhívás javítása

--- src/hu/bme/mit/asteroid/network/NetworkListener.java
+++ src/hu/bme/mit/asteroid/network/NetworkListener.java
@@ -8,20 +8,19 @@ import hu.bme.mit.asteroid.network.NetworkDiscover.NetworkDiscoverListener;
 import hu.bme.mit.asteroid.network.NetworkHelper.NetworkConnectionListener;
 import hu.bme.mit.asteroid.network.NetworkHelper.NetworkReceiveListener;
 
-public class NetworkListener implements NetworkConnectionListener, NetworkReceiveListener<Object>,
-		NetworkDiscoverListener {
+public class NetworkListener implements NetworkConnectionListener, NetworkDiscoverListener {
 
 	private final NetworkReceiveListener<GameState> mGameStateListener = new NetworkReceiveListener<GameState>() {
 		@Override
 		public void onReceive(GameState gameState) {
-			onReceive(gameState);
+			NetworkListener.this.onReceive(gameState);
 		}
 	};
 
 	private final NetworkReceiveListener<ControlEvent> mControlEventListener = new NetworkReceiveListener<ControlEvent>() {
 		@Override
 		public void onReceive(ControlEvent event) {
-			onReceive(event);
+			NetworkListener.this.onReceive(event);
 		}
 	};
 
@@ -37,17 +36,6 @@ public class NetworkListener implements NetworkConnectionListener, NetworkReceiv
 	}
 
 	public void onReceive(ControlEvent data) {
-	}
-
-	@Override
-	public void onReceive(Object data) {
-		if (data instanceof ControlEvent) {
-			onReceive(data);
-		} else if (data instanceof GameState) {
-			onReceive(data);
-		} else {
-			throw new IllegalArgumentException();
-		}
 	}
 
 	@Override
