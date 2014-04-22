f76585bf28d98c62257c31b3c15bf5445bc473b5
martonbalogh <balogh.marton@gmail.com>
2014-04-22 13:47:47 +0200

    Storage olvasó funkció megírva

--- src/hu/bme/mit/asteroid/Storage.java
+++ src/hu/bme/mit/asteroid/Storage.java
@@ -2,14 +2,41 @@ package hu.bme.mit.asteroid;
 
 import hu.bme.mit.asteroid.model.ToplistItem;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.InvalidPropertiesFormatException;
+import java.util.Properties;
 
 /**
  * A perzisztens adatok tárolását, kezelését végző osztály
  */
 public class Storage {
 
+	private static final int TOPLIST_SIZE = 10;
 	private static Storage sInstance = null;
+	private static File sFile = new File("config.xml");
+	private static FileInputStream sFileInputStream;
+	private static Properties sDefaultProperties;
+
+	private Storage() {
+		try {
+			sFileInputStream = new FileInputStream(sFile);
+		} catch (FileNotFoundException e) {
+			System.out.println("Couldn't open file.");
+		}
+		sDefaultProperties = new Properties();
+		sDefaultProperties.setProperty("level.HighestUnlocked", "0");
+		for (int i = 0; i < TOPLIST_SIZE; i++) {
+			String key = "toplist.Names." + i;
+			sDefaultProperties.setProperty(key, "-empty-");
+			key = "toplist.Score." + i;
+			sDefaultProperties.setProperty(key, "0");
+		}
+	}
 
 	/**
 	 * Az egyetlen {@link Storage} példány elkérése
@@ -29,8 +56,15 @@ public class Storage {
 	 * @return A legmagasabb sorszámú feloldott pálya száma
 	 */
 	public static int getHighestUnlockedLevel() {
-		// TODO beolvasás fájlból
-		return 0;
+		Properties properties = new Properties(sDefaultProperties);
+		try {
+			properties.loadFromXML(sFileInputStream);
+		} catch (Exception e) {
+			System.out.println("Cannot load from file.");
+			e.printStackTrace();
+		}
+		String highestUnlocked = properties.getProperty("level.HighestUnlocked");
+		return Integer.getInteger(highestUnlocked, 0);
 	}
 
 	/**
@@ -52,7 +86,20 @@ public class Storage {
 	 */
 	public static ArrayList<ToplistItem> getToplistItems() {
 		ArrayList<ToplistItem> toplistItems = new ArrayList<>();
-		// TODO beolvasás fájlból
+		Properties properties = new Properties(sDefaultProperties);
+		try {
+			properties.loadFromXML(sFileInputStream);
+		} catch (Exception e) {
+			System.out.println("Cannot load from file.");
+			e.printStackTrace();
+		}
+		for (int i = 0; i < TOPLIST_SIZE; i++) {
+			String key = "toplist.Names." + i;
+			String name = properties.getProperty(key);
+			key = "toplist.Score." + i;
+			String point = properties.getProperty(key);
+			toplistItems.add(new ToplistItem(name, Integer.getInteger(point)));
+		}
 		return toplistItems;
 	}
 
