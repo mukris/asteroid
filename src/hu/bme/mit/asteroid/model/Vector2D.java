4e54fa36a1805011fdc88fc422da6e628f612d8c
mukris <kristof.muki@mailbox.hu>
2014-04-27 04:24:28 +0100

    bugfix: vektor szögének és hosszának helyes számítása

--- src/hu/bme/mit/asteroid/model/Vector2D.java
+++ src/hu/bme/mit/asteroid/model/Vector2D.java
@@ -9,7 +9,7 @@ import java.io.Serializable;
  */
 public class Vector2D implements Cloneable, Serializable {
 	private static final long serialVersionUID = 7407514700895793824L;
-	
+
 	private float mX;
 	private float mY;
 
@@ -64,8 +64,9 @@ public class Vector2D implements Cloneable, Serializable {
 	}
 
 	public void setDirection(double direction) {
-		mX = (float) (getLength() * Math.cos(direction));
-		mY = (float) (getLength() * Math.sin(direction));
+		final float length = getLength();
+		mX = (float) (length * Math.cos(direction));
+		mY = (float) (length * Math.sin(direction));
 	}
 
 	public float getLength() {
@@ -73,61 +74,61 @@ public class Vector2D implements Cloneable, Serializable {
 	}
 
 	public void setLength(float length) {
-		mX = (float) (length * Math.cos(getDirection()));
-		mY = (float) (length * Math.sin(getDirection()));
+		final double direction = getDirection();
+		mX = (float) (length * Math.cos(direction));
+		mY = (float) (length * Math.sin(direction));
 	}
-	
+
 	static float getDistance(Vector2D v1, Vector2D v2) {
-		return (float) Math.sqrt((v1.mX - v2.mX) * (v1.mX -v2.mX) + (v1.mY - v2.mY) * (v1.mY - v2.mY));
+		return (float) Math.sqrt((v1.mX - v2.mX) * (v1.mX - v2.mX) + (v1.mY - v2.mY) * (v1.mY - v2.mY));
 	}
-	
-	//véletlen koordinátát generál a megadott térrészben
-	//int mert a képpontok intek
-	//4 térrészben generál random pontot, a végén ebből a 4 térrészből választ egyet
-	//nincs jobb ötletem
-	
+
+	// véletlen koordinátát generál a megadott térrészben
+	// int mert a képpontok intek
+	// 4 térrészben generál random pontot, a végén ebből a 4 térrészből választ
+	// egyet
+	// nincs jobb ötletem
+
 	public void randomPositionGenerator(int minDistance) {
 		int maxX = AsteroidGame.WINDOW_SIZE_X;
 		int maxY = AsteroidGame.WINDOW_SIZE_Y;
 		int centerX = maxX / 2;
 		int centerY = maxY / 2;
-		
+
 		int[] tmp_mX = new int[3];
 		int[] tmp_mY = new int[3];
-		
-			//felső rész X koordináta
-			tmp_mX[0] = (int)(Math.random() * maxX);
-			//felső rész Y koordináta
-			tmp_mY[0] = (int)(Math.random() * (centerY - minDistance));
-			
-			//bal oldal X koordináta
-			tmp_mX[1] = (int)(Math.random() * (centerX - minDistance));
-			//bal oldal Y koordináta
-			tmp_mY[1] = (int)(Math.random() * maxY);
-			
-			//jobb oldal X koordináta
-			tmp_mX[2] = (centerX + minDistance) + (int)(Math.random() * ((maxX - (centerX + minDistance)) + 1));
-			//jobb oldal Y koordináta
-			tmp_mY[2] = (int)(Math.random() * maxY);
-			
-			//alsó rész X koordináta
-			tmp_mX[3] = (int)(Math.random() * maxX);
-			//alsó rész Y koordináta
-			tmp_mY[3] = (centerY + minDistance) + (int)(Math.random() * ((maxY - (centerY + minDistance)) + 1));
-			
-			//0-4 generál egy egész számot, hogy kiválassza a térrészt
-			int i = (int)(Math.random() * 4);
-			
-			mX = (float) tmp_mX[i];
-			mY = (float) tmp_mY[i];
+
+		// felső rész X koordináta
+		tmp_mX[0] = (int) (Math.random() * maxX);
+		// felső rész Y koordináta
+		tmp_mY[0] = (int) (Math.random() * (centerY - minDistance));
+
+		// bal oldal X koordináta
+		tmp_mX[1] = (int) (Math.random() * (centerX - minDistance));
+		// bal oldal Y koordináta
+		tmp_mY[1] = (int) (Math.random() * maxY);
+
+		// jobb oldal X koordináta
+		tmp_mX[2] = (centerX + minDistance) + (int) (Math.random() * ((maxX - (centerX + minDistance)) + 1));
+		// jobb oldal Y koordináta
+		tmp_mY[2] = (int) (Math.random() * maxY);
+
+		// alsó rész X koordináta
+		tmp_mX[3] = (int) (Math.random() * maxX);
+		// alsó rész Y koordináta
+		tmp_mY[3] = (centerY + minDistance) + (int) (Math.random() * ((maxY - (centerY + minDistance)) + 1));
+
+		// 0-4 generál egy egész számot, hogy kiválassza a térrészt
+		int i = (int) (Math.random() * 4);
+
+		mX = (float) tmp_mX[i];
+		mY = (float) tmp_mY[i];
 	}
-	
-	
-	//véletlen irányt generál. az irány lehet a window bármely pontja
+
+	// véletlen irányt generál. az irány lehet a window bármely pontja
 	public void randomDirectionGenerator() {
 		mX = (float) (Math.random() * AsteroidGame.WINDOW_SIZE_X);
 		mY = (float) (Math.random() * AsteroidGame.WINDOW_SIZE_Y);
 	}
-	
 
 }
