f1bf851fd6ba4765bc37a0f6e9448571a8bc417d
mukris <kristof.muki@mailbox.hu>
2014-05-13 22:53:42 +0200

    Toplista frissítése megjelenésekor

--- src/hu/bme/mit/asteroid/gui/Toplist.java
+++ src/hu/bme/mit/asteroid/gui/Toplist.java
@@ -6,74 +6,89 @@ import hu.bme.mit.asteroid.model.ToplistItem;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
-import java.awt.Font;
 import java.awt.GridLayout;
 import java.util.ArrayList;
 import java.util.Collections;
 
-import javax.swing.JButton;
 import javax.swing.JLabel;
 
 /**
  * A toplista megjelenítését végző panel
  */
 public class Toplist extends GamePanel {
-	private static final long serialVersionUID = -2546252829715261063L;
+	private static final long serialVersionUID = -2252378497305833635L;
+
+	private Container mToplistTable = new Container();
+	private final JLabel mLabelToplist;
+	private final JLabel mLabelHeaderRank;
+	private final JLabel mLabelHeaderName;
+	private final JLabel mLabelHeaderScore;
 
 	public Toplist(GameWindow gameWindow) {
 		super(gameWindow);
 
-		final Container toplistTable = new Container();
+		mLabelToplist = new JLabel("Toplista");
+		mLabelToplist.setFont(mHeaderFont);
+		mLabelToplist.setHorizontalAlignment(JLabel.CENTER);
 
-		final ArrayList<ToplistItem> toplistItems = Storage.getInstance().getToplistItems();
-		Collections.reverse(toplistItems);
+		mLabelHeaderRank = new JLabel("Helyezés");
+		mLabelHeaderRank.setFont(mHeaderFont);
+		mLabelHeaderRank.setHorizontalAlignment(JLabel.CENTER);
 
-		final JLabel labelToplist = new JLabel("Toplista");
-		final JLabel jl_1 = new JLabel("Helyezés");
-		final JLabel jl_2 = new JLabel("Név");
-		final JLabel jl_3 = new JLabel("Pontszám");
+		mLabelHeaderName = new JLabel("Név");
+		mLabelHeaderName.setFont(mHeaderFont);
+		mLabelHeaderName.setHorizontalAlignment(JLabel.CENTER);
 
-		labelToplist.setFont(mHeaderFont);
-		labelToplist.setHorizontalAlignment(JLabel.CENTER);
+		mLabelHeaderScore = new JLabel("Pontszám");
+		mLabelHeaderScore.setFont(mHeaderFont);
+		mLabelHeaderScore.setHorizontalAlignment(JLabel.CENTER);
 
-		jl_1.setFont(mHeaderFont);
-		jl_1.setHorizontalAlignment(JLabel.CENTER);
+		update(mToplistTable);
 
-		jl_2.setFont(mHeaderFont);
-		jl_2.setHorizontalAlignment(JLabel.CENTER);
-
-		jl_3.setFont(mHeaderFont);
-		jl_3.setHorizontalAlignment(JLabel.CENTER);
-
-		toplistTable.add(jl_1);
-		toplistTable.add(jl_2);
-		toplistTable.add(jl_3);
-
-		int i = 0;
-		for (ToplistItem toplistItem : toplistItems) {
-			final JLabel labelRank = new JLabel(++i + ".");
-			labelRank.setHorizontalAlignment(JLabel.CENTER);
-			labelRank.setFont(mStandardFont);
-			toplistTable.add(labelRank);
-
-			final JLabel labelName = new JLabel(toplistItem.getName());
-			labelName.setHorizontalAlignment(JLabel.CENTER);
-			labelName.setFont(mStandardFont);
-			toplistTable.add(labelName);
-
-			final JLabel labelScore = new JLabel(toplistItem.getPoints() + "");
-			labelScore.setHorizontalAlignment(JLabel.CENTER);
-			labelScore.setFont(mStandardFont);
-			toplistTable.add(labelScore);
-		}
-
-		toplistTable.setLayout(new GridLayout(11, 3));
+		mToplistTable.setLayout(new GridLayout(11, 3));
 
 		final BorderLayout borderLayout = new BorderLayout(50, 30);
 		setLayout(borderLayout);
 
-		add(labelToplist, BorderLayout.NORTH);
-		add(toplistTable, BorderLayout.CENTER);
+		add(mLabelToplist, BorderLayout.NORTH);
+		add(mToplistTable, BorderLayout.CENTER);
 		add(getBackButton(PanelId.GAME_MODE_SELECTOR), BorderLayout.SOUTH);
+	}
+
+	@Override
+	protected void onShow() {
+		super.onShow();
+		update(mToplistTable);
+	}
+
+	private void update(Container toplistTable) {
+		final ArrayList<ToplistItem> toplistItems = Storage.getInstance().getToplistItems();
+		Collections.reverse(toplistItems);
+
+		toplistTable.removeAll();
+
+		mToplistTable.add(mLabelHeaderRank);
+		mToplistTable.add(mLabelHeaderName);
+		mToplistTable.add(mLabelHeaderScore);
+
+		int i = 1;
+		for (ToplistItem toplistItem : toplistItems) {
+			final JLabel labelRank = new JLabel(i++ + ".");
+			labelRank.setHorizontalAlignment(JLabel.CENTER);
+			labelRank.setFont(mStandardFont);
+			mToplistTable.add(labelRank);
+
+			final JLabel labelName = new JLabel(toplistItem.getName());
+			labelName.setHorizontalAlignment(JLabel.CENTER);
+			labelName.setFont(mStandardFont);
+			mToplistTable.add(labelName);
+
+			final JLabel labelScore = new JLabel(toplistItem.getPoints() + "");
+			labelScore.setHorizontalAlignment(JLabel.CENTER);
+			labelScore.setFont(mStandardFont);
+			mToplistTable.add(labelScore);
+		}
+		toplistTable.revalidate();
+		repaint();
 	}
 }
