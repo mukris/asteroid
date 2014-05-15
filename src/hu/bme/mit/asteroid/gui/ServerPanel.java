d7d6272fb0f1b8720c682b7e9b94cd4ead169913
mukris <kristof.muki@mailbox.hu>
2014-05-15 15:59:06 +0200

    ServerPanel layoutja helyrerakva

--- src/hu/bme/mit/asteroid/gui/ServerPanel.java
+++ src/hu/bme/mit/asteroid/gui/ServerPanel.java
@@ -7,12 +7,16 @@ import hu.bme.mit.asteroid.network.NetworkDiscover;
 import hu.bme.mit.asteroid.network.NetworkListener;
 import hu.bme.mit.asteroid.network.NetworkServer;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.util.List;
 
 import javax.swing.DefaultListModel;
+import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
@@ -23,7 +27,7 @@ import javax.swing.ListSelectionModel;
  */
 public class ServerPanel extends GamePanel {
 	private static final long serialVersionUID = 5452872136300248715L;
-	
+
 	private DefaultListModel<String> mAddresses;
 	private JList<String> mAddressList;
 
@@ -45,18 +49,48 @@ public class ServerPanel extends GamePanel {
 
 	public ServerPanel(GameWindow gameWindow) {
 		super(gameWindow);
-		
+
+		JLabel labelIPs = new JLabel("A szerver ismert IP címei:");
+		labelIPs.setFont(mStandardFont);
+		labelIPs.setHorizontalAlignment(JLabel.CENTER);
+
 		mAddresses = new DefaultListModel<>();
 		mAddressList = new JList<>(mAddresses);
 		mAddressList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		mAddressList.setVisibleRowCount(10);
 		mAddressList.setEnabled(false);
-		
+		mAddressList.setFont(mStandardFont);
+
 		JScrollPane listScrollPane = new JScrollPane(mAddressList);
+
+		setLayout(new GridBagLayout());
+		GridBagConstraints constraints = new GridBagConstraints();
+		constraints.gridx = 0;
+		constraints.gridy = 0;
+		constraints.ipadx = 200;
+		constraints.weighty = 1;
+		constraints.weightx = 1;
+		constraints.anchor = GridBagConstraints.PAGE_START;
+		add(getHeaderLabel("Várakozás játékos kapcsolódására"), constraints);
 		
-		add(getHeaderLabel("Szerver"));
-		add(listScrollPane);
-		add(getBackButton(PanelId.MULTIPLAYER_PANEL));
+		constraints.gridy = 1;
+		constraints.weighty = 0;
+		constraints.fill = GridBagConstraints.HORIZONTAL;
+		constraints.anchor = GridBagConstraints.CENTER;
+		add(labelIPs, constraints);
+		
+		constraints.gridy = 2;
+		constraints.weighty = 1;
+		constraints.fill = GridBagConstraints.BOTH;
+		constraints.insets = new Insets(0, 30, 30, 30);
+		add(listScrollPane, constraints);
+
+		constraints.gridy = 3;
+		constraints.ipady = 30;
+		constraints.fill = GridBagConstraints.HORIZONTAL;
+		constraints.anchor = GridBagConstraints.PAGE_END;
+		constraints.insets = new Insets(0, 30, 30, 30);
+		add(getBackButton(PanelId.MULTIPLAYER_PANEL), constraints);
 	}
 
 	@Override
@@ -82,7 +116,7 @@ public class ServerPanel extends GamePanel {
 			mGameWindow.showPanel(PanelId.MULTIPLAYER_PANEL);
 			e.printStackTrace();
 		}
-		
+
 		mAddresses.clear();
 		List<InetAddress> ownIPs = mNetworkServer.getOwnIPs();
 		for (InetAddress inetAddress : ownIPs) {
