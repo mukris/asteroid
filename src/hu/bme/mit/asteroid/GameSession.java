3d2f0e6e8a7ecf7d712ddd70761fa162f29fd5ad
bauertomi <tamas.bau@gmail.com>
2014-05-15 19:20:22 +0200

    nem megy - ba a pont

--- src/hu/bme/mit/asteroid/GameSession.java
+++ src/hu/bme/mit/asteroid/GameSession.java
@@ -408,7 +408,7 @@ public abstract class GameSession implements ControlInterface.Callback {
 				for (Weapon weapon : weapons) {
 					weapon.decreaseTimeUntilDeath(timeDelta);
 					if (weapon.isAlive()) {
-						Vector2D displacement = weapon.getSpeed().clone().multiply(timeDelta / 1000f);
+						Vector2D displacement = weapon.getSpeed().clone().multiply(timeDelta / 100f);
 						weapon.getPosition().add(displacement).inRange(mWidth, mHeight);
 					} else {
 						deadWeapons.add(weapon);
@@ -628,7 +628,9 @@ public abstract class GameSession implements ControlInterface.Callback {
 		protected void reducePoints (long timeDelta, Player.State state){
 			if ((mReducePointTime -= timeDelta) < 0){
 				mReducePointTime = 1000;
+				if(state.getPoints() > 0){
 			state.addPoints(-5);
+				}
 			}
 		}
 
